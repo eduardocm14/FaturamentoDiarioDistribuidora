143168b4fe762535aeb8cd994dffbad22c38eac1
eduardocm14 <eduardo.costa.info@gmail.com>
2024-02-07 23:32:36 -0300

    Dado um vetor que guarda o valor de faturamento diário de uma distribuidora de todos os dias de um ano, faça um programa, na linguagem que desejar, que calcule e retorne:
    • O menor valor de faturamento ocorrido em um dia do ano;
    • O maior valor de faturamento ocorrido em um dia do ano;
    • Número de dias no ano em que o valor de faturamento diário foi superior à média anual.
    
    a) Considerar o vetor já carregado com as informações de valor de faturamento.
    b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média.
    c) Utilize o algoritmo mais veloz que puder definir.

---
+++ FaturamentoDiario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaturamentoDiarioDistribuidora
+{
+    public class FaturamentoDiario
+    {
+        public double valor { get; set; }
+        public DateTime data { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace FaturamentoDiarioDistribuidora
 {
@@ -9,24 +10,38 @@ namespace FaturamentoDiarioDistribuidora
         static void Main(string[] args)
         {
             Random randNum = new();
-            var listaResultado = new List<double>();
+            //var listaResultado = new List<double>();
 
             DateTime dataInicial = DateTime.Now;
             DateTime dataFinal = new(2024, 12, 31);
 
             var diasAno = dataFinal.DayOfYear;
-
             var diaMesAno = dataInicial;
+            var quantidadeDiasUteis = 0;
+            //FaturamentoDiario[] vetorFaturamentoDiario = new FaturamentoDiario[dataFinal.Subtract(dataInicial).Days];
+            List<FaturamentoDiario> listagem =  new();
+            List<double> listaAleatorios = new();
+
+            Console.WriteLine("*******************Inicío Faturamento Anual*******************");
 
             for (int i = 0; i < dataFinal.Subtract(dataInicial).Days; i++)
             {
-                var numeroAleatorio = randNum.Next(100, 20000);
-                listaResultado.Add(numeroAleatorio);
+                FaturamentoDiario faturamentoDiario = new();
+                var numeroAleatorio = randNum.Next(100, 50000);
+                listaAleatorios.Add(numeroAleatorio);
 
                 if (diaMesAno.DayOfWeek != DayOfWeek.Saturday && diaMesAno.DayOfWeek != DayOfWeek.Sunday)
                 {
-                    //Console.WriteLine(diaMesAno.DayOfWeek + " - " + DayOfWeek.Saturday);
-                    Console.WriteLine(diaMesAno.ToString("dd/MM/yyyy") + "(" + diaMesAno.DayOfWeek + ") - " + numeroAleatorio.ToString("F"));
+                    faturamentoDiario.data = diaMesAno;
+                    faturamentoDiario.valor = numeroAleatorio;
+
+                    listagem.Add(faturamentoDiario);
+
+                    Console.WriteLine($"Data: {faturamentoDiario.data:dd/MM/yyyy} " +
+                                      $"Dia da Semana: {diaMesAno.DayOfWeek} " +
+                                      $"Valor: {faturamentoDiario.valor:n2}");
+
+                    quantidadeDiasUteis ++;
                 }
 
                 diaMesAno = diaMesAno.AddDays(1);
@@ -48,7 +63,58 @@ namespace FaturamentoDiarioDistribuidora
                 //                  DateTime.IsLeapYear(dec31.Year) ? "(Leap Year)" : "");
             }
 
-            //Console.WriteLine($"Quantidade de dias:{diasAno} do ano: {dataFinal.Year} ");
+            Console.WriteLine("*******************Fim Faturamento Anual*******************");
+
+            double valorMediaAnual = 0;
+            double maiorValor = 0;
+            double menorValor = 0;
+            DateTime dataMenorValor = DateTime.Now;
+            DateTime dataMaiorValor = DateTime.Now;
+
+            //foreach (var item in listaAleatorios)
+            //{
+            //    Console.WriteLine($"Números Aleatorios: {item}");
+            //}
+
+            var contDiasFatDiarioMaiorMediaAnual = 0;
+
+            foreach (var item in listagem)
+            {
+                if (item != null)
+                {
+                    valorMediaAnual += item.valor;
+
+                    if (menorValor == 0)
+                        menorValor = item.valor;
+
+                    if (maiorValor == 0)
+                        maiorValor = item.valor;
+
+                    if (menorValor > item.valor)
+                    {
+                        menorValor = item.valor;
+                        dataMenorValor = item.data;
+                    }
+
+                    if (maiorValor < item.valor)
+                    {
+                        maiorValor = item.valor;
+                        dataMaiorValor = item.data;
+                    }
+
+                    if (item.valor > valorMediaAnual / quantidadeDiasUteis)
+                    {
+                        contDiasFatDiarioMaiorMediaAnual ++;
+                    }
+                }
+            }
+               
+            Console.WriteLine($"Faturamento Anual: {valorMediaAnual:n2}");
+            Console.WriteLine($"Faturamento Média Anual: {valorMediaAnual / quantidadeDiasUteis:n2}");
+            Console.WriteLine($"Faturamento >> Maior Valor: {maiorValor:n2} - dia: {dataMaiorValor}");
+            Console.WriteLine($"Faturamento >> Menor Valor: {menorValor:n2} - dia: {dataMenorValor}");
+            Console.WriteLine($"Número de dias no ano em que o valor de faturamento diário foi superior à média anual: {contDiasFatDiarioMaiorMediaAnual}");
+            Console.WriteLine($"Quantidade de dias:{diasAno} / dias uteis:{quantidadeDiasUteis} do ano: {dataFinal.Year} ");
         }
     }
 }
